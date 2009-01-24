7b4851137dafb86c7267151f64e798cccc815405
cpitis <cpitis@834bb202-e8be-11dd-9d8c-a70aa0a93a20>
2009-01-24 17:44:58 +0000

    Added specialization for three dimension vector.

---
+++ trunk/implementation/geometry/include/algebra/vector_3.hpp
@@ -0,0 +1,70 @@
+#ifndef ALGEBRA_VECTOR_3_HPP
+#define ALGEBRA_VECTOR_3_HPP
+
+namespace algebra
+{
+
+template< typename U, typename UT>
+class vector< 3, U, UT>: public details::vector_base< 3, U, UT>
+{
+	typedef vector< 3, U, UT> my_type_;
+public:
+	vector()
+	{
+		v_[0] = v_[1] = v_[2] = unit_traits_type::zero();
+	}
+
+	template< typename It> vector( It begin, It end)
+	{
+		assert( std::distance( begin, end) == DIMENSIONS);
+		std::copy( begin, end, &v_[0]);
+	}
+
+	template< typename It> vector( It begin)
+	{
+		for( unsigned n = 0; n < DIMENSIONS; ++n, ++begin)
+		{
+			v_[n] = *begin;
+		}
+	}
+
+	// TODO: Test
+	vector( const U& v0, const U& v1, const U& v2)
+	{
+		v_[0] = v0;
+		v_[1] = v1;
+		v_[2] = v2;
+	}
+
+
+	friend my_type_ operator+( const my_type_& op1, const my_type_& op2)
+	{
+		return my_type_( op1.v_[0] + op2.v_[0], op1.v_[1] + op2.v_[1], op1.v_[2] + op2.v_[2]);
+	}
+
+	my_type_& operator+=( const my_type_& op)
+	{
+		v_[0] += op.v_[0];
+		v_[1] += op.v_[1];
+		v_[2] += op.v_[2];
+		return *this;
+	}
+
+	friend my_type_ operator-( const my_type_& op1, const my_type_& op2)
+	{
+		return my_type_( op1.v_[0] - op2.v_[0], op1.v_[1] - op2.v_[1], op1.v_[2] - op2.v_[2]);
+	}
+
+	my_type_& operator-=( const my_type_& op)
+	{
+		v_[0] -= op.v_[0];
+		v_[1] -= op.v_[1];
+		v_[2] -= op.v_[2];
+		return *this;
+	}
+
+};
+
+} // namespace algebra
+
+#endif // ALGEBRA_VECTOR_3_HPP
