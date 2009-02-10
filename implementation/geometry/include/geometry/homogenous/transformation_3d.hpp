cbc6bf11975f2a6842f7525e07c3a694671adc70
cpitis <cpitis@834bb202-e8be-11dd-9d8c-a70aa0a93a20>
2009-02-10 17:03:24 +0000

    Added support for rotating around a direction that goes through origin (3D, homogenous coordinates).

--- implementation/geometry/include/geometry/homogenous/transformation_3d.hpp
+++ implementation/geometry/include/geometry/homogenous/transformation_3d.hpp
@@ -2,6 +2,7 @@
 #define GEOMETRY_HOMOGENOUS_TRANSFORMATION_3_HPP
 
 #include "geometry/transformation.hpp"
+#include "geometry/direction_concept.hpp"
 #include <cmath>
 
 namespace geometry
@@ -70,6 +71,29 @@ public:
 			sin,  cos, 0, 0,
 			  0,    0, 1, 0,
 			  0,    0, 0, 1
+			);
+	}
+
+	/// \sa http://en.wikipedia.org/wiki/Rotation_matrix#Rotation_about_an_arbitrary_vector
+	template< typename Dir>
+	static typename boost::enable_if< boost::is_same< typename Dir::tag, direction_tag>, transformation>::type
+		rotation( const Dir& direction, const unit_type& angle)
+	{
+		BOOST_CONCEPT_ASSERT( (Direction< Dir>));
+		unit_type 
+			cos = std::cos( angle),
+			sin = std::sin( angle);
+		unit_type 
+			lx = direction.dx(), ly = direction.dy(), lz = direction.dz(), 
+			lx2 = lx*lx, ly2 = ly*ly, lz2 = lz*lz,
+			lxly = lx*ly, lxlz = lx*lz, lylz = ly*lz,
+			lxsin = lx*sin, lysin = ly*sin, lzsin = lz*sin;
+		
+		return transformation(
+			lx2+(1-lx2)*cos,    lxly*(1-cos)-lzsin,  lxly*(1-cos)+lysin, 0,
+			lxly*(1-cos)+lzsin, ly2+(1-ly2)*cos,     lylz*(1-cos)-lxsin, 0,
+			lxlz*(1-cos)-lysin, lylz*(1-cos)+lxsin,  lz2+(1-lz2)*cos,    0,
+			0,                  0,                   0,                  1
 			);
 	}
 
