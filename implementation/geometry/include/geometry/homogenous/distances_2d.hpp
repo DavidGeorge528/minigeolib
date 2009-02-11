17916ecb527a2be8dde32088de3bc43fe608dff9
cpitis <cpitis@834bb202-e8be-11dd-9d8c-a70aa0a93a20>
2009-02-11 06:31:39 +0000

    Implemented distance between two vertices for homogenous 2D and 3D coordinates.

---
+++ implementation/geometry/include/geometry/homogenous/distances_2d.hpp
@@ -0,0 +1,27 @@
+#ifndef GEOMETRY_HOMOGENOUS_DISTANCES_2D_HPP
+#define GEOMETRY_HOMOGENOUS_DISTANCES_2D_HPP
+
+#include "geometry/coord_system_concept.hpp"
+#include "geometry/coord_system_tags.hpp"
+#include "geometry/impl/enablers.hpp"
+#include "geometry/vertex_concept.hpp"
+#include <boost/concept/assert.hpp>
+
+namespace geometry
+{
+
+template< typename V>
+typename impl::enabled_for< typename V::coord_system, 2, hcoord_system_tag, typename V::coord_system::length_type>::type  
+	distance( const V& vertex1, const V& vertex2)
+{
+	BOOST_CONCEPT_ASSERT( (coord_system_concept<typename V::coord_system>));
+	BOOST_CONCEPT_ASSERT( (HomogenousVertex2D<V>));
+	typename V::unit_type 
+		dx = vertex2.x() / vertex2.w() - vertex1.x() / vertex1.w(),
+		dy = vertex2.y() / vertex2.w() - vertex1.y() / vertex1.w();
+	return std::sqrt( dx*dx + dy*dy);
+}
+
+} // geometry
+
+#endif // GEOMETRY_HOMOGENOUS_DISTANCES_2D_HPP
---
+++ implementation/geometry_tests/geometry/hdistance_2d_tests.cpp
@@ -0,0 +1,51 @@
+#include "geometry/homogenous/distances_2d.hpp"
+#include "geometry/homogenous/distances_3d.hpp" // Keep this include, to check on SFINAE
+#include "geometry/homogenous/hcoord_system.hpp"
+#include "geometry/homogenous/vertex_2d.hpp"
+#include "../tests_common.hpp"
+#include "../test_traits.hpp"
+#include <boost/mpl/list.hpp>
+#include <boost/bind.hpp>
+#include <algorithm>
+
+namespace 
+{
+
+using namespace geometry;
+
+typedef boost::mpl::list< 
+	vertex< hcoord_system< 2, float, algebra::unit_traits< float> > >,
+	vertex< hcoord_system< 2, double, algebra::unit_traits< double> > >
+> tested_vertices;
+
+BOOST_AUTO_TEST_CASE_TEMPLATE( test_distance, V, tested_vertices)
+{
+	typedef V vertex;
+	typedef typename vertex::unit_type unit_type;
+	typedef typename vertex::coord_system::length_type length_type;
+
+	vertex v1( 10, 20, 10), v2( -1, -2);
+	length_type expected_distance = std::sqrt( unit_type(4+16));
+	ALGTEST_CHECK_EQUAL_UNIT( expected_distance, distance( v1, v2));
+	ALGTEST_CHECK_EQUAL_UNIT( unit_type(0), distance( v1, v1));
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+	
+BOOST_AUTO_TEST_CASE_TEMPLATE( test_distance_as_predicate, V, tested_vertices)
+{
+	using namespace boost;
+
+	typedef V vertex;
+	typedef typename vertex::unit_type unit_type;
+	typedef typename vertex::coord_system::length_type length_type;
+
+	vertex v1( 10, 20, 10);
+	std::list< vertex> lv;
+	lv.push_back( vertex(0,0,0));
+	lv.push_back( vertex(2,2,2));
+	std::vector< unit_type> distances( lv.size());
+	std::transform( lv.begin(), lv.end(), distances.begin(), bind( distance<vertex>, _1, v1));
+}
+
+} // namespace
