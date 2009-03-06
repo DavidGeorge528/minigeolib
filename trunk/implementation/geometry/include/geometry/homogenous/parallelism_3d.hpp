43082149eb1fa23a0b5186ab8130e76a85e12f15
cpitis <cpitis@834bb202-e8be-11dd-9d8c-a70aa0a93a20>
2009-03-06 21:10:46 +0000

    Implemented parallelism checking between two lines.

--- trunk/implementation/geometry/include/geometry/homogenous/parallelism_3d.hpp
+++ trunk/implementation/geometry/include/geometry/homogenous/parallelism_3d.hpp
@@ -2,6 +2,7 @@
 #define GEOMETRY_PARALLELISM_3D_HPP
 
 #include "geometry/direction_concept.hpp"
+#include "geometry/line_concept.hpp"
 #include "algebra/tolerance_policy_concept.hpp"
 #include "algebra/epsilon_tolerance.hpp"
 #include <boost/concept/assert.hpp>
@@ -12,24 +13,40 @@ namespace geometry
 
 /// \brief It checks whether two given directions are parallel or not.
 /// \tparam Dir the direction type, implementing the 3D direction concept.
-/// \tparam T the tolerance policy to be used for comparing the calculation results with the expected results.
+/// \tparam TP the tolerance policy to be used for comparing the calculation results with the expected results.
 /// \param d1 the first direction to be compared.
 /// \param d2 the second direction to be compared.
 /// \param tolerance the tolerance to be used for comparing the calculation results with the expected results.
 /// \details
 ///		It calculates the cosinus of the angle between the two given directions. If the absolute value of that cosinus 
 ///		is 1 (considering the given tolerance), it means that the two directions are parallel.
-template< typename Dir, typename T>
+template< typename Dir, typename TP>
 typename boost::enable_if< impl::is_direction< Dir, 3>, bool>::type 
-	are_parallel( const Dir& d1, const Dir& d2, const T& tolerance)
+	are_parallel( const Dir& d1, const Dir& d2, const TP& tolerance)
 {
 	typedef typename Dir::unit_type unit_type;
-	BOOST_CONCEPT_ASSERT( (algebra::TolerancePolicy< T>));
+	BOOST_CONCEPT_ASSERT( (algebra::TolerancePolicy< TP>));
 	BOOST_CONCEPT_ASSERT( (Direction3D<Dir>));
 	// We know that the norm of direction concept is always 1, so we "simply" the computation, by making only the dot 
 	// product:
 	const unit_type cos_angle = d1.dx()*d2.dx() + d1.dy()*d2.dy() + d1.dz()*d2.dz();
 	return tolerance.equals( std::abs( cos_angle), unit_type( 1));
+}
+
+
+/// \brief It checks whether two given lines are parallel or not.
+/// \tparam L the line type, implementing the 3D line concept.
+/// \tparam TP the tolerance policy to be used for comparing the calculation results with the expected results.
+/// \param d1 the first line to be compared.
+/// \param d2 the second line to be compared.
+/// \param tolerance the tolerance to be used for comparing the calculation results with the expected results.
+/// \details
+///		It uses the direction parallelism checking function, providing the directions of the two involved lines.
+template< typename L, typename TP>
+typename boost::enable_if< impl::is_line< L, 3>, bool>::type
+	are_parallel( const L& l1, const L& l2, const TP& tolerance)
+{
+	return are_parallel( l1.dir(), l2.dir(), tolerance);
 }
 
 } // namespace geometry
--- trunk/implementation/geometry_tests/geometry/parallelism_3d_tests.cpp
+++ trunk/implementation/geometry_tests/geometry/parallelism_3d_tests.cpp
@@ -1,6 +1,8 @@
 #include "geometry/homogenous/parallelism_3d.hpp"
 #include "geometry/homogenous/hcoord_system.hpp"
 #include "geometry/homogenous/direction_3d.hpp"
+#include "geometry/homogenous/vertex_3d.hpp"
+#include "geometry/line.hpp"
 #include "../tests_common.hpp"
 #include "../test_traits.hpp"
 #include <boost/mpl/list.hpp>
@@ -15,6 +17,10 @@ typedef hcoord_system< 3, double> hcoord_system_double;
 typedef boost::mpl::list< 
 	direction< hcoord_system_float>, 
 	direction< hcoord_system_double> > tested_directions;
+
+typedef boost::mpl::list<
+	boost::mpl::list< line< hcoord_system_float>, vertex< hcoord_system_float>, direction< hcoord_system_float> >,
+	boost::mpl::list< line< hcoord_system_double>, vertex< hcoord_system_double>, direction< hcoord_system_double> > > tested_lines;
 
 // ---------------------------------------------------------------------------------------------------------------------
 
@@ -33,6 +39,32 @@ BOOST_AUTO_TEST_CASE_TEMPLATE( test_parallel_directions, D, tested_directions)
 	BOOST_CHECK( !are_parallel( d3, d4, tolerance));
 	BOOST_CHECK( are_parallel( d1, d1, tolerance));
 	BOOST_CHECK( are_parallel( d4, d4, tolerance));
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+BOOST_AUTO_TEST_CASE_TEMPLATE( test_parallel_lines, T, tested_lines)
+{
+	using namespace boost::mpl;
+
+	typedef typename at<T,int_<0> >::type line_type;
+	typedef typename line_type::unit_type unit_type;
+	typedef typename at<T,int_<1> >::type vertex_type;
+	typedef typename at<T,int_<2> >::type direction_type;
+
+	vertex_type base1( 1, 2, 3), base2( 5, 6, 7), base3( 10, 9, 8), base4( 20, 30, 10);
+	direction_type d1( 10, 20, 30), d2( 1, 2, 3), d3( -1, -2, -3), d4( 11, 12, 13);
+	line_type l1( base1, d1), l2( base2, d2), l3( base3, d3), l4( base4, d4);
+
+	algebra::epsilon_tolerance< unit_type> tolerance( 1e-6);
+	BOOST_CHECK( are_parallel( l1, l2, tolerance));
+	BOOST_CHECK( are_parallel( l1, l3, tolerance));
+	BOOST_CHECK( are_parallel( l2, l3, tolerance));
+	BOOST_CHECK( !are_parallel( l1, l4, tolerance));
+	BOOST_CHECK( !are_parallel( l2, l4, tolerance));
+	BOOST_CHECK( !are_parallel( l3, l4, tolerance));
+	BOOST_CHECK( are_parallel( l1, l1, tolerance));
+	BOOST_CHECK( are_parallel( l4, l4, tolerance));
 }
 
 } // namespace
